34356e170dcc75edc7b8b85acf5251e9d51cb06e
Adrian Padin <padin.adrian@gmail.com>
2019-07-31 22:19:44 -0500

    Migrating test code into embedded_tools and cleaning up.

--- n64_dualshock.ino
+++ n64_dualshock.ino
@@ -10,40 +10,9 @@
 #include "embedded_tools/n64/adrian_single_wire_arduino.hpp"
 #include "embedded_tools/adrian_helpers.hpp"
 
-
-/* ===== Helper Functions ===== */
-
-#define read_bit()\
-{\
-    PORTD &= 0xF7;\
-    button_status |= (((PIND & 4) >> 2) << index);\
-    PORTD |= 0x08;\
-}
-
-#define zero_bit()\
-{\
-    PORTD &= 0xFB;\
-    i++; i++; i++; i++; i++;\
-    PORTD |= 0x04;\
-    i++;\
-}
-
-#define one_bit()\
-{\
-    PORTD &= 0xFB;\
-    i++;\
-    PORTD |= 0x04;\
-    i++; i++; i++; i++; i++;\
-}
-
-#define stop_bit()\
-{\
-    PORTD &= 0xFB;\
-    i++; i++; i++;\
-    PORTD |= 0x04;\
-    i++; i++; i++;\
-}
-
+using adrian::zero_bit;
+using adrian::one_bit;
+using adrian::stop_bit;
 
 /* ===== Setup ===== */
 
@@ -63,7 +32,6 @@ void loop()
     pinMode(2, OUTPUT);
 
     uint32_t button_status = 0;
-    volatile int i = 0;
 
     zero_bit(); // 0
     zero_bit(); // 1
@@ -73,14 +41,13 @@ void loop()
     zero_bit(); // 5
     zero_bit(); // 6
     zero_bit(); // 7
-
     stop_bit();
 
     pinMode(2, INPUT_PULLUP);
 
-    for (uint8_t index = 0; index < 32; index++)
+    for (uint32_t mask = 1; mask > 0; mask <<= 1)
     {
-        read_bit();
+        button_status |= ((PIND & 4) ? mask : 0);
     }
 
     Serial.println(button_status);
