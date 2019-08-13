095ddd53ff6a98429c2efdfbf57ed9a0ed4e36ff
Adrian Padin <padin.adrian@gmail.com>
2019-08-12 20:22:06 -0500

    Work in progress.

--- n64_dualshock.ino
+++ n64_dualshock.ino
@@ -14,6 +14,7 @@ using adrian::zero_bit;
 using adrian::one_bit;
 using adrian::stop_bit;
 
+
 /* ===== Setup ===== */
 
 void setup()
@@ -27,11 +28,14 @@ void setup()
 
 /* ===== Loop =====  */
 
+int loop_counter = 0;
 void loop()
 {
     pinMode(2, OUTPUT);
 
     uint32_t button_status = 0;
+    uint32_t mask = 1;
+    volatile uint8_t i = 0;
 
     zero_bit(); // 0
     zero_bit(); // 1
@@ -40,17 +44,46 @@ void loop()
     zero_bit(); // 4
     zero_bit(); // 5
     zero_bit(); // 6
-    zero_bit(); // 7
+    one_bit(); // 7
     stop_bit();
 
-    pinMode(2, INPUT_PULLUP);
+    // Configure the pins for input
+    //   DDRC = DDRC & Mask;
 
-    for (uint32_t mask = 1; mask > 0; mask <<= 1)
+    //   // Enable the pullups
+    //   PORTC = PORTC | Mask;
+
+    DDRD &= 0xFB;       // Configure as input
+    PORTD |= 0x04;      // Set to high (enable pull-up)
+
+    // pinMode(2, INPUT_PULLUP);
+
+    // i = 1; i = 2; i = 3; i = 4;
+    // i = 1; i = 2; i = 3; i = 4;
+    // i = 1; i = 2; i = 3; i = 4;
+    // i = 1; i = 2; i = 3; i = 4;
+    for (; mask > 0; mask <<= 1)
     {
+        PORTD |= 0x08;
         button_status |= ((PIND & 4) ? mask : 0);
+        PORTD &= 0xF7;
+        i = 1; i = 2; i = 3; i = 4; i = 5;
+        i = 1; i = 2; i = 3; i = 4; i = 5;
+        i = 1; i = 2; i = 3; i = 4; i = 5;
+        i = 1; i = 2; i = 3; i = 4; i = 5;
+        i = 1;
     }
 
-    Serial.println(button_status);
+    // Copy button info
+    // Example:
+    ButtonState buttons;
+    memcpy(&buttons, &button_status, sizeof(buttons));
+
+    Serial.print(loop_counter++);
+    Serial.print(": ");
+
+
+    // Serial.println(button_status, HEX);
 
     // Wait
     delay(100);
