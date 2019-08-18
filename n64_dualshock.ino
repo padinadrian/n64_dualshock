3a0c2b219d83675751312527e0373273c157113e
Adrian Padin <padin.adrian@gmail.com>
2019-08-18 00:35:01 -0500

    First fully working N64 example.
    * Finally able to read N64 controller button status reliably.
    * Created example ino file to demonstrate working behavior.
    * Minor updates to embedded_tools.

--- n64_dualshock.ino
+++ n64_dualshock.ino
@@ -8,11 +8,15 @@
 /* ===== Includes ====== */
 #include <Arduino.h>
 #include "embedded_tools/n64/adrian_single_wire_arduino.hpp"
+#include "embedded_tools/n64/adrian_n64_controller.hpp"
 #include "embedded_tools/adrian_helpers.hpp"
+#include "embedded_tools/ps2/adrian_dualshock.hpp"
 
 using adrian::zero_bit;
 using adrian::one_bit;
 using adrian::stop_bit;
+using adrian::N64Controller;
+using adrian::DualShock;
 
 
 /* ===== Setup ===== */
@@ -25,65 +29,21 @@ void setup()
     pinMode(3, OUTPUT);
 }
 
+static unsigned char reverse_lookup[16] = {
+0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe,
+0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf, };
+
+uint8_t reverse(uint8_t n) {
+   // Reverse the top and bottom nibble then swap them.
+   return (reverse_lookup[n&0b1111] << 4) | reverse_lookup[n>>4];
+}
+
 
 /* ===== Loop =====  */
 
 int loop_counter = 0;
 void loop()
 {
-    pinMode(2, OUTPUT);
-
-    uint32_t button_status = 0;
-    uint32_t mask = 1;
-    volatile uint8_t i = 0;
-
-    zero_bit(); // 0
-    zero_bit(); // 1
-    zero_bit(); // 2
-    zero_bit(); // 3
-    zero_bit(); // 4
-    zero_bit(); // 5
-    zero_bit(); // 6
-    one_bit(); // 7
-    stop_bit();
-
-    // Configure the pins for input
-    //   DDRC = DDRC & Mask;
-
-    //   // Enable the pullups
-    //   PORTC = PORTC | Mask;
-
-    DDRD &= 0xFB;       // Configure as input
-    PORTD |= 0x04;      // Set to high (enable pull-up)
-
-    // pinMode(2, INPUT_PULLUP);
-
-    // i = 1; i = 2; i = 3; i = 4;
-    // i = 1; i = 2; i = 3; i = 4;
-    // i = 1; i = 2; i = 3; i = 4;
-    // i = 1; i = 2; i = 3; i = 4;
-    for (; mask > 0; mask <<= 1)
-    {
-        PORTD |= 0x08;
-        button_status |= ((PIND & 4) ? mask : 0);
-        PORTD &= 0xF7;
-        i = 1; i = 2; i = 3; i = 4; i = 5;
-        i = 1; i = 2; i = 3; i = 4; i = 5;
-        i = 1; i = 2; i = 3; i = 4; i = 5;
-        i = 1; i = 2; i = 3; i = 4; i = 5;
-        i = 1;
-    }
-
-    // Copy button info
-    // Example:
-    ButtonState buttons;
-    memcpy(&buttons, &button_status, sizeof(buttons));
-
-    Serial.print(loop_counter++);
-    Serial.print(": ");
-
-
-    // Serial.println(button_status, HEX);
 
     // Wait
     delay(100);
